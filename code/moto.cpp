e8fb7cfc2bafffaaae66455ecbadd81b7f7bea70
ashkorehennessy <ashkorehennessy@gmail.com>
2025-02-27 15:16:13 +0800

    更新moto

--- code/moto.cpp
+++ code/moto.cpp
@@ -3,3 +3,38 @@
 //
 
 #include "moto.h"
+#include "config.h"
+Moto::Moto(int pwmNum, int dirgpio, int encoderNum, int encgpio)
+    : motor(pwmNum), direction(dirgpio), encoder(encoderNum, encgpio), speed(0)
+{
+    motor.set_frequency(15000);
+    motor.set_duty(0);
+    motor.enable();
+    direction.setDirection("out");
+    direction.setValue(false);
+
+}
+
+void Moto::set_speed(int _speed)
+{
+    if (_speed > PWM_DUTY_MAX) {
+        _speed = PWM_DUTY_MAX;
+    } else if (_speed < -PWM_DUTY_MAX) {
+        _speed = -PWM_DUTY_MAX;
+    }
+    if (_speed > 0)
+    {
+        direction.setValue(false);
+    }
+    else
+    {
+        direction.setValue(true);
+        _speed = -_speed;
+    }
+    motor.set_duty(_speed);
+}
+
+void Moto::update_speed()
+{
+    speed = encoder.pulse_counter_update();
+}
--- inc/moto.h
+++ inc/moto.h
@@ -5,4 +5,20 @@
 #ifndef MOTO_H
 #define MOTO_H
 
+#include <pwm.h>
+#include <encoder.h>
+#include "GPIO.h"
+
+class Moto {
+public:
+    Moto(int pwmNum, int dirgpio, int encoderNum, int encgpio);
+    void set_speed(int _speed);
+    void update_speed();
+    int speed;
+private:
+    PWM motor;
+    GPIO direction;
+    ENCODER encoder;
+};
+
 #endif //MOTO_H
