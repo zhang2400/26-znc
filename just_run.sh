92ef45b820e4221f2cc17ef0ab8f801bc1b4ce08
ashkorehennessy <ashkorehennessy@gmail.com>
2026-05-02 04:45:41 +0800

    移动main.cpp到src

---
+++ just_run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ip="10.100.16.169"
+#ip="192.168.188.169"
+#ip="192.168.43.253"
+username="loongson"
+# 上传到开发板并运行
+echo "Running :ssh $username@$ip sudo pkill cv"
+ssh -t $username@$ip "sudo pkill cv"
+sleep 0.5
+echo "Running :ssh $username@$ip ./cv"
+ssh -t $username@$ip "sudo ./cv"
